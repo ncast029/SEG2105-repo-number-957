3f26c45e484d5cd9f16db0c8835205cf2ee6cf68
Tomas F Fieg <tfieg050@uottawa.ca>
2020-10-31 18:26:48 -0400

    added UI for create, edit, and remove service

--- app/src/main/java/com/example/appversion2/AdminWelcomePage.java
+++ app/src/main/java/com/example/appversion2/AdminWelcomePage.java
@@ -2,13 +2,21 @@ package com.example.appversion2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class AdminWelcomePage extends AppCompatActivity {
 
     TextView welcomeAdmin;
     String welcome;
+
+    private Button create_Service_Button;
+    private Button edit_service_button;
+    private Button remove_service_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +25,32 @@ public class AdminWelcomePage extends AppCompatActivity {
         welcomeAdmin = findViewById(R.id.tvWelcomeAdmin);
         welcome = getIntent().getExtras().getString("Value");
         welcomeAdmin.setText("Welcome " + welcome + ", you are logged in as admin.");
+
+        create_Service_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(CustomerRegistration.this, MainActivity.class));
+            }
+        });
+
+        edit_service_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(CustomerRegistration.this, MainActivity.class));
+            }
+        });
+
+        remove_service_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(CustomerRegistration.this, MainActivity.class));
+            }
+        });
+    }
+
+    private void setupUIViews() {
+        create_Service_Button = (Button)findViewById(R.id.CreateServiceButton);
+        edit_service_button = (Button)findViewById(R.id.EditServiceButton);
+        remove_service_button = (Button)findViewById(R.id.RemoveServiceButton);
     }
 }
