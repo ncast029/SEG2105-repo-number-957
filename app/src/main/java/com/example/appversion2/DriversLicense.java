499815ab8e9dd162c3849f26c1a786d9007234d2
Nicolas Castro <ncast029@uottawa.ca>
2020-10-31 14:37:59 -0400

    More work on services

--- app/src/main/java/com/example/appversion2/DriversLicense.java
+++ app/src/main/java/com/example/appversion2/DriversLicense.java
@@ -1,8 +1,11 @@
 package com.example.appversion2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.widget.LinearLayout;
@@ -30,9 +33,23 @@ import com.google.firebase.auth.FirebaseUser;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class DriversLicense extends AppCompatActivity {
 
+    private String rate;
+    private ArrayList<File> files;
+
+    public String getRate() {
+        return rate;
+    }
+    public void setRate(String newRate) {
+        rate = newRate;
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
+        rate = "120.00";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_license_registration);
 
@@ -66,7 +83,180 @@ public class DriversLicense extends AppCompatActivity {
 
             }
         });
+
+        Button photoButton = (Button) findViewById(R.id.uibtn);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View view) {
+                final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+                AlertDialog.Builder builder = new AlertDialog.Builder(DriversLicense.this);
+                builder.setTitle("Upload Photo");
+                builder.setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        if (options[item].equals("Take Photo"))
+                        {
+                            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            File f = new File("", "temp.jpg");
+                            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                            startActivityForResult(intent, 1);
+                        }
+                        else if (options[item].equals("Choose from Gallery"))
+                        {
+                            Intent intent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            startActivityForResult(intent, 2);
+                        }
+                        else if (options[item].equals("Cancel")) {
+                            dialog.dismiss();
+                        }
+                    }
+                });
+                builder.show();
+            }
+        });
+
+        /*protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (resultCode == RESULT_OK) {
+                if (requestCode == 1) {
+                    File f = new File(Environment.getExternalStorageDirectory().toString());
+                    for (File temp : f.listFiles()) {
+                        if (temp.getName().equals("temp.jpg")) {
+                            f = temp;
+                            break;
+                        }
+                    }
+                    try {
+                        Bitmap bitmap;
+                        BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+                        bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(), bitmapOptions);
+                        bitmap=getResizedBitmap(bitmap, 400);
+                        IDProf.setImageBitmap(bitmap);
+                        BitMapToString(bitmap);
+                        String path = android.os.Environment
+                                .getExternalStorageDirectory()
+                                + File.separator
+                                + "Phoenix" + File.separator + "default";
+                        f.delete();
+                        OutputStream outFile = null;
+                        File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+                        try {
+                            outFile = new FileOutputStream(file);
+                            bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                            outFile.flush();
+                            outFile.close();
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else if (requestCode == 2) {
+                    Uri selectedImage = data.getData();
+                    String[] filePath = { MediaStore.Images.Media.DATA };
+                    Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+                    c.moveToFirst();
+                    int columnIndex = c.getColumnIndex(filePath[0]);
+                    String picturePath = c.getString(columnIndex);
+                    c.close();
+                    Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                    thumbnail=getResizedBitmap(thumbnail, 400);
+                    Log.w("path of image from gallery......******************.........", picturePath+"");
+                    IDProf.setImageBitmap(thumbnail);
+                    BitMapToString(thumbnail);
+                }
+            }
+        }
+        public String BitMapToString(Bitmap userImage1) {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            userImage1.compress(Bitmap.CompressFormat.PNG, 60, baos);
+            byte[] b = baos.toByteArray();
+            Document_img1 = Base64.encodeToString(b, Base64.DEFAULT);
+            return Document_img1;
+        }
+
+        public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+            int width = image.getWidth();
+            int height = image.getHeight();
+
+            float bitmapRatio = (float)width / (float) height;
+            if (bitmapRatio > 1) {
+                width = maxSize;
+                height = (int) (width / bitmapRatio);
+            } else {
+                height = maxSize;
+                width = (int) (height * bitmapRatio);
+            }
+            return Bitmap.createScaledBitmap(image, width, height, true);
+        } */
+
+        Button fileButton = (Button) findViewById(R.id.filebtn);
+        fileButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View view) {
+
+            }
+        });
+
+        Button submitButton = (Button) findViewById(R.id.btnSubmit);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View view) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(DriversLicense.this);
+                builder.setCancelable(true);
+                builder.setTitle("Confirm Driver's License Form Submission");
+                builder.setMessage("Once your application is processed $" + rate + " will be charged to your account.");
+                builder.setPositiveButton("Confirm",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                boolean success = submitForm(((EditText)findViewById(R.id.dobField)).getText().toString(), ((EditText)findViewById(R.id.addrField)).getText().toString());
+                                if (!success) {
+                                    AlertDialog.Builder builder2 = new AlertDialog.Builder(DriversLicense.this);
+                                    builder2.setCancelable(false);
+                                    builder2.setTitle("ERROR");
+                                    builder2.setMessage("An error has occurred while processing your request. Please try again later.");
+                                    builder2.setPositiveButton("Ok",
+                                            new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    finish();
+                                                }
+                                            });
+
+
+                                    AlertDialog dialog2 = builder2.create();
+                                    dialog2.show();
+                                } else {
+                                    finish();
+                                }
+                            }
+                        });
+                builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {}
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+        });
     }
+
+    private boolean submitForm(String dob, String address) {
+        try {
+            //String firstName = ;
+            //String lastName = ;
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 
 
 
---
+++ app/src/main/java/com/example/appversion2/ImageViewer.java
@@ -0,0 +1,17 @@
+package com.example.appversion2;
+
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.io.File;
+
+public class ImageViewer extends AppCompatActivity {
+
+    private ArrayList<File> images;
+
+    public ImageViewer(ArrayList<File> images, Intent intent) {
+        //come back to this once you have enough for tomas
+    }
+
+}
